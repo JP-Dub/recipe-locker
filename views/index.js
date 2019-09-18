561999f3b77e1ac01dfdbcd5542e76f17f4e12af
Glitch (hello-express) <none>
2019-09-18 20:43:08 +0000

    💫⏲ Checkpoint ./public/style.css:553280/5752 ./views/index.js:553280/1788

--- views/index.js
+++ views/index.js
@@ -10,27 +10,40 @@ class Main extends Component {
   
   render() {
     return (
-      <div id ="main" className="container">#main / .container
-        <header id='locker'>
-            <p>The Recipe Locker</p>
-          </header>
-          <div id='flex'>
-            <div id='index' className='contents'>
-              <table>
-                <tr><td>foo gartarum</td></tr>
-                <tr><td>lipsum chiken</td></tr>
-                <tr><td>roast modicum</td></tr>
-                <tr><td>cranium</td></tr>
-              </table>
-            </div>
-            <div id='recipe' className='contents'>
-              <header id='title'>Title</header>
-              <article id='ingredients'>
-                Ingredients
-              </article>
-            </div>
-          </div>
+<div id ="main" className="container">
+  <header id='locker'>
+    <p>The Recipe Locker</p>
+  </header>
+  <div id='flex'>
+    <div id='index' className='contents'>
+      <header id='index-name'>Recipe Name</header>
+      <div id='index-table'>
+        <table>
+          <tbody>
+            <tr><td>foo gartarum</td></tr>
+            <tr><td>lipsum chiken</td></tr>
+            <tr><td>roast modicum</td></tr>
+            <tr><td>cranium</td></tr>
+            <tr><td>goo fartarum</td></tr>
+            <tr><td>cipsum lhiken</td></tr>
+            <tr><td>moast rodicum</td></tr>
+            <tr><td>cranium</td></tr>
+            <tr><td>foo gartarum</td></tr>
+            <tr><td>lipsum chiken</td></tr>
+            <tr><td>roast modicum</td></tr>
+            <tr><td>cranium</td></tr>
+          </tbody>
+        </table>
       </div>
+    </div>
+    <div id='recipe' className='contents'>
+      <header id='recipe-name'>Yum Yum Chicken</header>
+      <article id='recipe-ingredients'>
+        Ingredients
+      </article>       
+    </div>
+  </div>
+</div>
       );
   }
 }
