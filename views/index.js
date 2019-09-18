0118ad6c1b771639caf8f5dae40669f0e7aae5b9
Glitch (hello-express) <none>
2019-09-18 22:06:33 +0000

    🛀🕍 Checkpoint ./public/style.css:553280/304 ./views/index.js:553280/41

--- views/index.js
+++ views/index.js
@@ -20,7 +20,7 @@ class Main extends Component {
       <ErrorBoundary>
         <div id ="main" className="container">
           <header id='locker'>
-            <p>The Recipe Locker<span id='edit'><i className='far fa-edit'></i></span></p>
+            <p /><p>The Recipe Locker</p><p id='edit'><i className='far fa-edit'></i></p>
           </header>
           <div id='flex'>
             <div id='index' className='contents'>
