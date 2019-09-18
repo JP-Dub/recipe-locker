f8ce29ca51568e143f8575feb5b7bfbca9622688
Glitch (hello-express) <none>
2019-09-18 21:43:10 +0000

    ✋🎲 Checkpoint ./views/index.js:553280/1277

--- views/index.js
+++ views/index.js
@@ -6,50 +6,57 @@ import '../public/style.css';
 class Main extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      
+    }
   }
+  
+  componentDidMount() {}
+  
+  componentWillUnMount() {}
   
   render() {
     return (
       <ErrorBoundary>
-      <div id ="main" className="container">
-        <header id='locker'>
-          <p>The Recipe Locker</p>
-        </header>
-        <div id='flex'>
-          <div id='index' className='contents'>
-            <header id='index-name'>Recipe Name</header>
-            <div id='index-table'>
-              <table>
-                <tbody>
-                  <tr><td>foo gartarum</td></tr>
-                  <tr><td>lipsum chiken</td></tr>
-                  <tr><td>roast modicum</td></tr>
-                  <tr><td>Yum Yum Chicken</td></tr>
-                  <tr><td>goo fartarum</td></tr>
-                  <tr><td>cipsum lhiken</td></tr>
-                  <tr><td>moast rodicum</td></tr>
-                  <tr><td>cranium</td></tr>
-                  <tr><td>foo gartarum</td></tr>
-                  <tr><td>lipsum chiken</td></tr>
-                  <tr><td>roast modicum</td></tr>
-                  <tr><td>cranium</td></tr>
-                </tbody>
-              </table>
+        <div id ="main" className="container">
+          <header id='locker'>
+            <p>The Recipe Locker<span id='edit'>&#xf044;</span></p>
+          </header>
+          <div id='flex'>
+            <div id='index' className='contents'>
+              <header id='index-name'>Recipe Name</header>
+              <div id='index-table'>
+                <table>
+                  <tbody>
+                    <tr><td>foo gartarum</td></tr>
+                    <tr><td>lipsum chiken</td></tr>
+                    <tr><td>roast modicum</td></tr>
+                    <tr><td>Yum Yum Chicken</td></tr>
+                    <tr><td>goo fartarum</td></tr>
+                    <tr><td>cipsum lhiken</td></tr>
+                    <tr><td>moast rodicum</td></tr>
+                    <tr><td>cranium</td></tr>
+                    <tr><td>foo gartarum</td></tr>
+                    <tr><td>lipsum chiken</td></tr>
+                    <tr><td>roast modicum</td></tr>
+                    <tr><td>cranium</td></tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+            <div id='recipe' className='contents'>
+              <header id='recipe-name'>Yum Yum Chicken</header>
+              <article id='recipe-ingredients'>
+                <ul>Ingredients:
+                  <li>2lbs Chicken Breast</li>
+                  <li>1 can of diced tomatoes</li>
+                  <li>1 pinch of salt</li>
+                  <li>1/2 teaspoon white pepper</li>
+                </ul>
+              </article>       
             </div>
           </div>
-          <div id='recipe' className='contents'>
-            <header id='recipe-name'>Yum Yum Chicken</header>
-            <article id='recipe-ingredients'>
-              <ul>Ingredients
-                <li>2lbs Chicken Breast</li>
-                <li>1 can of diced tomatoes</li>
-                <li>1 pinch of salt</li>
-                <li>1/2 teaspoon white pepper</li>
-              </ul>
-            </article>       
-          </div>
         </div>
-      </div>
       </ErrorBoundary>
       );
   }
