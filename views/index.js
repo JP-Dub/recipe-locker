1ad32e832352ec51e91e9202afa33c922225bb78
Glitch (hello-express) <none>
2019-09-23 18:00:14 +0000

    👨‍❤️‍👨🏞 Checkpoint ./views/index.js:553280/146

--- views/index.js
+++ views/index.js
@@ -7,7 +7,7 @@ class Main extends Component {
   constructor(props) {
     super(props);
     this.userLogin = this.userLogin.bind(this);
-    this.twitterHandler = this.twitterHandler.bind(this);
+    // this.twitterHandler = this.twitterHandler.bind(this);
     
     this.state = {
       locked : 1,
@@ -120,7 +120,7 @@ class Main extends Component {
             </div>
           </div>
         </div>
-        <Login />
+        {!this.state.locked? (<Login />) : (<div/>)}
       </ErrorBoundary>
       );
   }
