dc301211319574556f5f05b26ab86e9134a74287
Glitch (hello-express) <none>
2019-10-09 15:09:54 +0000

    🧀🏤 Checkpoint ./views/index.js:553280/1853

--- views/index.js
+++ views/index.js
@@ -172,63 +172,6 @@ class App extends Component {
   }
 }
 
-// class Login extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.twitterHandler = this.twitterHandler.bind(this);
-//     this.state = {};
-//   }
-//   componentDidMount() {}
-
-//   componentWillUnMount() {}
-
-//   twitterHandler(evt) {
-//     evt.preventDefault();
-//     window.location.href = "/api/auth/twitter";
-//   }
-
-//   render() {
-//     return (
-//       <ErrorBoundary>
-//         <div id="main-center">
-//           <div id="main-account" className="main-container">
-//             <header id="account-header" className="ua-header">
-//               <h3>User Account</h3>
-//               <i
-//                 id="account-close"
-//                 className="fas fa-window-close lckr"
-//                 title="close"
-//                 onClick={this.props.userLogin}
-//               />
-//             </header>
-//             <div id="twitter" className="flex-container">
-//               <header id="twitter-header" className="ua-header">
-//                 <h3>Twitter Login</h3>
-//                 <i
-//                   class="fab fa-twitter"
-//                   title="sign in"
-//                   onClick={this.twitterHandler}
-//                 />
-//               </header>
-//               <div id="signin-info">
-//                 <p>Welcome to the Recipe Locker!</p>
-//                 <p>
-//                   {" "}
-//                   If would like to add, create and edit or delete your own
-//                   recipes, we ask that you sign in with Twitter. This will also
-//                   prevent others from modifying or deleting your recipes. To do
-//                   this, just click on the Twitter icon in the upper corner.
-//                 </p>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </ErrorBoundary>
-//     );
-//   }
-// }
-
-
 // configure ajax call
 const ajax = {
   ready: function ready(fn) {
