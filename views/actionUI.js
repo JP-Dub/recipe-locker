54e4ecf8221deac1912171776e77fd4005900e7a
Glitch (hello-express) <none>
2019-11-06 19:18:19 +0000

    🍜🌺 Checkpoint ./views/actionUI.js:553280/5133 ./public/actionUI.css:553280/133

--- views/actionUI.js
+++ views/actionUI.js
@@ -53,86 +53,87 @@ class ActionUI extends Component {
 }
 
 class Login extends Component {
-  constructor(props) {
-    super(props);
-    this.twitterHandler = this.twitterHandler.bind(this);
-    this.state = {};
-  }
-  componentDidMount() {}
-
-  componentWillUnMount() {}
-
-  twitterHandler(evt) {
-    evt.preventDefault();
-    window.location.href = "/api/auth/twitter";
-  }
-
-  render() {
-    return (
-      <ErrorBoundary>
-            <div id="twitter" className="flex-container radius">
-              <header id="twitter-header" className="ua-header headr">
-                <h3>Twitter Login</h3>
-                <i            
-                  className="fab fa-twitter"
-                  title="User Login"
-                  onClick={this.twitterHandler}
-                />
-              </header>
-              <div id="signin-info">
-                <p>Welcome to the Recipe Locker!</p>
-                <p>              
-                  If would like to add, create and edit or delete your own
-                  recipes, we ask that you sign in with Twitter. This will also
-                  prevent others from modifying or deleting your recipes. To do
-                  this, just click on the Twitter icon in the upper corner.
-                </p>
+    constructor(props) {
+      super(props);
+      this.twitterHandler = this.twitterHandler.bind(this);
+      this.state = {};
+    }
+    componentDidMount() {}
+  
+    componentWillUnMount() {}
+  
+    twitterHandler(evt) {
+      evt.preventDefault();
+      window.location.href = "/api/auth/twitter";
+    }
+  
+    render() {
+      return (
+        <ErrorBoundary>
+              <div id="inner-subframe" className="flex-container radius">
+                <header id="actionUI-subheader" className="ua-header headr">
+                  <h3>Twitter Login</h3>
+                  <i
+                    id='twitter'
+                    className="fab fa-twitter"
+                    title="User Login"
+                    onClick={this.twitterHandler}
+                  />
+                </header>
+                <div id="signin-info">
+                  <p>Welcome to the Recipe Locker!</p>
+                  <p>              
+                    If would like to add, create and edit or delete your own
+                    recipes, we ask that you sign in with Twitter. This will also
+                    prevent others from modifying or deleting your recipes. To do
+                    this, just click on the Twitter icon in the upper corner.
+                  </p>
+                </div>
               </div>
-            </div>
-      </ErrorBoundary>
-    );
-  }
-}
-
-
-class DeleteRecipe extends Component {
-  constructor(props) {
-    super(props);
-    this.deleteRecipe = this.deleteRecipe.bind(this);
-    this.state = {};
+        </ErrorBoundary>
+      );
+    }
   }
   
-  deleteRecipe() {
-    console.log('delete the recipe')
-  }
   
-  render() {
-    console.log(this.props)
-    return (
-         <ErrorBoundary>
-            <div id="twitter" className="flex-container radius">
-              <header id="twitter-header" className="ua-header headr">
-                <h3>{this.props.recipeName}</h3>
-                <i
-                  id="delete"
-                  className="fas fa-trash"
-                  title="Confirm Delete"
-                  onClick={this.deleteRecipe}
-                />
-              </header>
-              <div id="signin-info">
-                <p>Attention: You are about to permenantly delete your recipe!</p>
-                <p>              
-                  If would like to continue with this action, confirm by clicking 
-                  on the trash can icon in the upper right hand corner. Otherwise, 
-                  please cancel by closing window.
-                </p>
+  class DeleteRecipe extends Component {
+    constructor(props) {
+      super(props);
+      this.deleteRecipe = this.deleteRecipe.bind(this);
+      this.state = {};
+    }
+    
+    deleteRecipe() {
+      console.log('delete the recipe')
+    }
+    
+    render() {
+      console.log(this.props)
+      return (
+           <ErrorBoundary>
+              <div id="inner-subframe" className="flex-container radius">
+                <header id="actionUI-subheader" className="ua-header headr">
+                  <h3>{this.props.recipeName}</h3>
+                  <i
+                    id="delete"
+                    className="fas fa-trash"
+                    title="Confirm Delete"
+                    onClick={this.deleteRecipe}
+                  />
+                </header>
+                <div id="signin-info">
+                  <p>Attention: You are about to permenantly delete your recipe!</p>
+                  <p>              
+                    If would like to continue with this action, confirm by clicking 
+                    on the trash can icon in the upper right hand corner. Otherwise, 
+                    please cancel by closing window.
+                  </p>
+                </div>
               </div>
-            </div>
-      </ErrorBoundary>
-    )
+        </ErrorBoundary>
+      )
+    }
   }
-}
 
 
 
